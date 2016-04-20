86faac17787695bcd6083d8c1b2adbc482e6bd4b
Daniel McBryde <mcbryded@gmail.com>
2016-04-20 18:25:06 -0500

    post to message room

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,7 +1,7 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default JSONAPIAdapter.extend({
+export default JSONAPIAdapter.extend(DataAdapterMixin, {
   authorizer: `authorizer:application`,
   host: `https://chatty-tn-api.herokuapp.com`,
   namespace: `api`,
