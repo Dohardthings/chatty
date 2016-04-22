15a1a7d3634c814e436a6b9a667fefe4c48eb143
Daniel McBryde <mcbryded@gmail.com>
2016-04-22 08:02:18 -0500

    push before hackathon

--- app/room/controller.js
+++ app/room/controller.js
@@ -1,6 +1,19 @@
 import Ember from 'ember';
+import { task, timeout } from 'ember-concurrency';
 
 export default Ember.Controller.extend({
+  init() {
+  this._super(...arguments);
+  this.get(`waitAFewSeconds`).perform();
+},
+
+waitAFewSeconds: task(function * () {
+  while(true) {
+    yield timeout(2000);
+    console.log(`polling`);
+    this.store.findAll(`message`, { include: `chatter` });
+  }
+}),
   newContent: ``,
 
   postMessage(newContent) {
